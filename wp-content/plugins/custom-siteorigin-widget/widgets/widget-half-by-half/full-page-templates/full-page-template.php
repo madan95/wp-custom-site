e00b9f5241725a60f7af47f85bda1a94f7559d4e
Madan <madan_limbu@hotmail.co.uk>
2018-06-11 22:52:55 +0100

    remove custom widget

--- wp-content/plugins/custom-siteorigin-widget/widgets/widget-half-by-half/full-page-templates/full-page-template.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <h1>Template called</h1>
-    <?php echo wp_kses_post($instance['some_text']); ?>
-    <?php echo wp_get_attachment_image(wp_kses_post($instance['some_media'])); ?>
-</div>
